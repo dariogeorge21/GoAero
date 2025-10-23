db436f0ff497afe332194e7bacd27d27bbfb7227
Dario George <edu.dariogeorge21@gmail.com>
2025-10-23 14:55:03 +0530

    Remove unused WARNING_ORANGE color constant for cleaner code

--- src/com/GoAero/ui/BookingManagementPanel.java
+++ src/com/GoAero/ui/BookingManagementPanel.java
@@ -27,7 +27,6 @@ public class BookingManagementPanel extends JPanel {
     private static final Color BACKGROUND_GRAY = new Color(250, 250, 250);
     private static final Color CARD_WHITE = Color.WHITE;
     private static final Color DANGER_RED = new Color(244, 67, 54);
-    private static final Color WARNING_ORANGE = new Color(255, 193, 7);
     private JTable bookingsTable;
     private DefaultTableModel tableModel;
     private JButton viewDetailsButton, updateStatusButton, updatePaymentButton, refreshButton;
